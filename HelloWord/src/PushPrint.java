87bb3ac88362e84243aa172c2fc5ffc185631d3a
ujbKDH <eogusdjddk@naver.com>
2016-11-08 14:39:22 +0900

    PushPrint.java modified on GitHub

--- HelloWord/src/PushPrint.java
+++ HelloWord/src/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with 365");
 		
 	}
 
