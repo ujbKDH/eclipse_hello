cff89a42764ae913f1bfd7c027d548fee50d4f44
ujbKDH <eogusdjddk@naver.com>
2016-11-08 15:16:11 +0900

    remote PushPrint.java modified

--- HelloWord/src/PushPrint.java
+++ HelloWord/src/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with 365");
+		System.out.println("remote master");
 		
 	}
 
